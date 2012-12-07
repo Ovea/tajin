299f144d244b668581e5fd0a8eef4006c5a37b91
altus34 <altus34@gmail.com>
2012-12-07 12:00:08 -0500

    Refactor test (use jasmine)

--- client/src/test/js/logging/logging_unit.js
+++ client/src/test/js/logging/logging_unit.js
@@ -1,20 +1,25 @@
-describe("Logger", function () {
+describe("Default Logger", function () {
 
-    it("should be installed", function () {
+    it("is installed", function () {
         expect(tajin.modules()).toContain("log");
     });
 
-    it("it should be at 'none' level by default", function () {
-        expect(tajin['log'].level()).toBe('none');
+    it("is at 'none' level by default", function () {
+        expect(tajin['log'].logger().level).toBe("none");
     });
 
-    it("and nothing is logged", function() {
+    it("and nothing can be logged", function () {
+        spyOn(console, 'log');
+
+        tajin.log.debug('a message', 'with param', 1);
+        tajin.log.info('a message', 'with param', 1);
+        tajin.log.warn('a message', 'with param', 1);
+        tajin.log.error('a message', 'with param', 1);
+
+        expect(console.log).not.toHaveBeenCalled();
+        expect(console.debug).not.toHaveBeenCalled();
 
     });
-
-
-//    existe default logger
-//    at level none
 
 
     // Ca create my own logger
@@ -22,8 +27,35 @@ describe("Logger", function () {
     // can log
 
 
-
     // when using config its visible in  tajin.options.<module name>.level
 
 });
+
+describe("Personal logger", function () {
+
+    var debug_logger = tajin.log.logger('Debug Logger', 'debug');
+
+    it("can create debug level logger", function () {
+        expect(debug_logger.level).toBe('debug');
+    });
+
+    it("all log messages are processed", function () {
+        spyOn(console, 'log');
+
+        debug_logger.debug('a message', 'with param', 1);
+
+        expect(console.log).toHaveBeenCalled();
+//        expect(console.log).toHaveBeenCalledWith('INFO [Debug Logger] a message with param 1');
+
+
+//        debug_logger.info('a message', 'with param', 1);
+//        debug_logger.warn('a message', 'with param', 1);
+//        debug_logger.error('a message', 'with param', 1);
+
+    });
+
+
+});
+
+
 
