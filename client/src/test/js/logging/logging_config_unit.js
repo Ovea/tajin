099de761108c6180f35a92388de37337e08342c5
altus34 <altus34@gmail.com>
2012-12-07 15:23:27 -0500

    Logging tests

--- client/src/test/js/logging/logging_config_unit.js
+++ client/src/test/js/logging/logging_config_unit.js
@@ -1,1 +1,30 @@
-// when using config its visible in  tajin.options.<module name>.level
+describe("Default Logger configured to WARN on initialization ", function () {
+
+    it("is installed", function () {
+        expect(tajin.modules()).toContain("log");
+    });
+
+    it("is at 'warn' level by default", function () {
+        expect(tajin.options.log.level).toBe("warn");
+    });
+
+    it("only 'warn' and 'error'  log messages are processed", function () {
+        spyOn(console, 'debug');
+        spyOn(console, 'info');
+        spyOn(console, 'warn');
+        spyOn(console, 'error');
+
+        tajin.log.debug('a message', 'with param', 1);
+        tajin.log.info('a message', 'with param', 1);
+        tajin.log.warn('a message', 'with param', 1);
+        tajin.log.error('a message', 'with param', 1);
+
+        expect(console.debug).not.toHaveBeenCalled();
+        expect(console.info).not.toHaveBeenCalled();
+        expect(console.warn).toHaveBeenCalledWith('WARN [tajin.log] a message', 'with param', 1);
+        expect(console.error).toHaveBeenCalledWith('ERROR [tajin.log] a message', 'with param', 1);
+    });
+
+
+
+});
