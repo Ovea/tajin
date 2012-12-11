d183f29ee6e137a3a317bca6827c2e65c548f16d
mathieucarbou <mathieu.carbou@gmail.com>
2012-12-11 00:23:57 -0500

    added jasmine specs + suite.html

--- client/src/test/js/config/config.spec.custom2.js
+++ client/src/test/js/config/config.spec.custom2.js
@@ -1,15 +1,7 @@
 describe("Config Module", function () {
 
-    it("loaded default config", function () {
+    it("loaded inexisting config does not prevent Tajin initalization", function () {
         expect(tajin.options.config.url).toContain('inexisting.json');
-    });
-
-});
-
-describe("Custom config", function () {
-
-    it("loaded", function () {
-        expect(tajin.options).toBeDefined();
     });
 
 });
