48f674c2bc1f36867344b230b479d96bea0f7bf7
mathieucarbou <mathieu.carbou@gmail.com>
2013-01-21 10:56:56 -0500

    validation check

--- framework/src/main/groovy/com/ovea/tajin/resources/TajinResourceManager.groovy
+++ framework/src/main/groovy/com/ovea/tajin/resources/TajinResourceManager.groovy
@@ -18,11 +18,15 @@ package com.ovea.tajin.resources
 import com.ovea.tajin.TajinConfig
 import groovy.json.JsonBuilder
 
+import java.util.logging.Logger
+
 /**
  * @author Mathieu Carbou (mathieu.carbou@gmail.com)
  * @date 2012-11-26
  */
 class TajinResourceManager {
+
+    private static final Logger LOGGER = Logger.getLogger(TajinResourceManager.name)
 
     final TajinConfig config
 
@@ -31,18 +35,20 @@ class TajinResourceManager {
     }
 
     Collection<File> getResources() {
-        //TODO MATHIEU - find dynami resources
+        //TODO MATHIEU - find dynamic resources
         return []
     }
 
     void buid() {
-        println 'Building: all...'
+        LOGGER.info("Rebuilding Tajin resources...")
         File output = new File(config.webapp, config.json.client?.output as String ?: 'tajin-client.json')
         output.text = new JsonBuilder(config.json.client?.modules ?: [:])
     }
 
-    void buid(File resource) {
-        println 'Building: ' + resource
+    void buid(File changed) {
+        //TODO MATHIEU - find file to rebuild for changed file
+        File res = changed
+        LOGGER.info("Rebuilding Tajin resource ${res}...")
     }
 
 }
