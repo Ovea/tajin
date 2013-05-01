8b67bc325fff74b20f9f44676f4fcdb915c92166
mathieucarbou <mathieu.carbou@gmail.com>
2013-05-01 01:35:59 -0400

    testing deployment on dev server

--- tajin-framework/framework/src/main/resources/com/ovea/tajin/framework/service.sh
+++ tajin-framework/framework/src/main/resources/com/ovea/tajin/framework/service.sh
@@ -1,23 +1,19 @@
-
 #!/bin/bash
 #
-# chkconfig: 345 99 05
-# description: Java deamon script
+# Copyright (C) 2011 Ovea <dev@ovea.com>
 #
-# A non-SUSE Linux start/stop script for Java daemons.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# Derived from -
-# Home page: http://www.source-code.biz
-# License:   GNU/LGPL (http://www.gnu.org/licenses/lgpl.html)
-# Copyright 2006 Christian d'Heureuse, Inventec Informatik AG, Switzerland.
+# http://www.apache.org/licenses/LICENSE-2.0
 #
-# History:
-# 2010-09-21 Josh Davis: Changed 'sudo' to 'su', fix some typos, removed unused variables
-# 2009-03-04 Josh Davis: Ubuntu/Redhat version.
-# 2006-06-27 Christian d'Heureuse: Script created.
-# 2006-07-02 chdh: Minor improvements.
-# 2006-07-10 chdh: Changes for SUSE 10.0.
-
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 # Set this to your Java installation
 JAVA_HOME=/usr/java/latest
